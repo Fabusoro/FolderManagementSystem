f560a45964a51c3eb557276dc9b496d6f7791065
Fabusoro <99407963+Fabusoro@users.noreply.github.com>
2022-12-11 18:16:45 +0100

    added domain objects

---
+++ FileSystem/FileSystem.Domain/Files.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Domain
+{
+    public class Files
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string FolderPath { get; set; }              
+    }
+}
---
+++ FileSystem/FileSystem.Domain/Folder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Domain
+{
+    public class Folder
+    {        
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string FolderName { get; set; }
+        public string FolderPath { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+    }
+}
